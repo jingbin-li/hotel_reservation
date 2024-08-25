c96b874d25a6532141430b166090bded24e72a14
jingbin.li <binupup@163.com>
2024-08-25 12:11:21 +0800

    remove controller

--- hrs_server/src/modules/guest/guest.module.ts
+++ hrs_server/src/modules/guest/guest.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
-import { GuestController } from './guest.controller';
 import { GuestService } from './guest.service';
 
 @Module({
-  controllers: [GuestController],
-  providers: [GuestService]
+  controllers: [],
+  providers: [GuestService],
 })
 export class GuestModule {}
